b3b5ab2e7b20fba36f69c2629c9854b9c6639fef
Gloziksoft <peto7724@gmail.com>
2025-06-26 09:49:32 +0200

    Prva zaloha

--- src/test/java/com/poistenie/app/PoistenieAppApplicationTests.java
+++ src/test/java/com/poistenie/app/PoistenieAppApplicationTests.java
@@ -2,6 +2,8 @@ package com.poistenie.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootTest
 class PoistenieAppApplicationTests {
@@ -9,5 +11,12 @@ class PoistenieAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@GetMapping("/test")
+	@ResponseBody
+	public String test() {
+		return "Hello world!";
+	}
+
 
 }
