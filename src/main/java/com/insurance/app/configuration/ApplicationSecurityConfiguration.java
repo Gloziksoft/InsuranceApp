a834a968f6557e35b80febd45d2ecdbe126b3850
Gloziksoft <peto7724@gmail.com>
2026-01-05 12:49:07 +0100

    docker-compose.yml 	modified:   src/main/java/com/insurance/app/configuration/ApplicationSecurityConfiguration.java 	modified:   src/main/resources/application-prod.properties

--- src/main/java/com/insurance/app/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/com/insurance/app/configuration/ApplicationSecurityConfiguration.java
@@ -30,7 +30,6 @@ public class ApplicationSecurityConfiguration {
                 )
                 .build();
     }
-
     // ===============================
     // 🔐 APPLICATION SECURITY
     // ===============================
